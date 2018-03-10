d8680fb4a2549da74a9599caba0c60e2523e6ff0
Mahadev <mahadev@Mahadev.local>
2018-03-10 12:34:31 +0530

    Add prod build mode webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 var config = {
-    entry: './main.js',
-    output: {
-        path: '/',
-        filename: 'index.js',
+  entry: './main.js',
+  output: {
+    path: '/',
+    filename: 'index.js',
+    path: __dirname + '/dist'
+  },
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader'
+      },],
+
     },
-    devServer: {
-        inline: true,
-        port: 8080
-    },
+      {
+        test: /\.css?$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }],
 
-    module: {
-        rules: [{
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader'
-                }, ],
-
-            },
-            {
-                test: /\.css?$/,
-                use: [{
-                  loader: 'style-loader'
-                },{
-                    loader:'css-loader'
-                } ],
-
-            }
-        ]
-    }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './index.html', fileName: `__dirname/index.html`,alwaysWriteToDisk: true})
+  ]
 }
 module.exports = config;
